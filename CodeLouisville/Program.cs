b65b8c4c879b9c42f08bf0dc92202b9a5d733330
Brittney <3944682+bawilc01@users.noreply.github.com>
2019-05-14 18:33:39 -0400

    Recopied into Code Louisville repo

--- CodeLouisville/Program.cs
+++ CodeLouisville/Program.cs
@@ -1,13 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CodeLouisville
+namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is my first edit");
-            Console.WriteLine("Hello World!");
+            int runningTotal = 0;
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                // Prompt user for minutes exercised
+                Console.Write("Enter how many minutes you exercised: ");
+                string entry = Console.ReadLine();
+
+                if (entry.ToUpper() == "Q")
+                {
+                    keepGoing = false;
+
+                }
+                else
+                {
+
+                    // Add minutes exercised to total
+                    int minutes = int.Parse(entry);
+                    try
+                    {
+                    }
+                    catch (
+                    )
+                    {
+                    }
+
+                    if (minutes <= 0)
+                    {
+                        Console.WriteLine(minutes + " is not an acceptable value.");
+                        continue;
+                    }
+                    else if (minutes <= 10)
+                    {
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go hot stuff!");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        Console.WriteLine("You must be a ninja warrior in training!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Okay, now you're just showing off!");
+                    }
+
+                    runningTotal = runningTotal + minutes;
+
+
+                    // Display total minutes exercised to the screen
+                    Console.WriteLine($"You've exercised a total of {runningTotal} minutes. Press Q to quit.");
+
+                }
+            }
+
+            Console.WriteLine("Goodbye!");
+            Console.ReadKey();
+
         }
     }
 }
+
